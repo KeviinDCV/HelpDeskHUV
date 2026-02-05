0f2ffaf094a5c8bb805b7135d4b72aa423bef669
KeviinDCV <110347632+KeviinDCV@users.noreply.github.com>
2026-02-05 10:41:45 -0500

    fx
    fx

---
+++ public/build/assets/index-D9tAhSoo.js
@@ -0,0 +1,6 @@
+import{a as o}from"./button-DWAcHH05.js";import{a as c}from"./app-DrGrfWp1.js";/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const s=[["path",{d:"M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2",key:"975kel"}],["circle",{cx:"12",cy:"7",r:"4",key:"17ys0d"}]],n=o("User",s);function i(r){const e=c.useRef({value:r,previous:r});return c.useMemo(()=>(e.current.value!==r&&(e.current.previous=e.current.value,e.current.value=r),e.current.previous),[r])}export{n as U,i as u};
